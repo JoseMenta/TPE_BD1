3edd709560f9b2f1f2ecade738fb501ae57a3e26
gfrancois <gfrancois@itba.edu.ar>
2022-11-17 00:12:51 -0300

    unificacion de archivos .sql

--- funciones.sql
+++ funciones.sql
@@ -1,3 +1,118 @@
+
+/*
+-- --------------------------------------------------------------------------------------------
+-- INSTRUCTIVO PARA CARGAR DATOS DE UN .CSV
+-- --------------------------------------------------------------------------------------------
+Para la importación de datos resulta necesario contar con acceso a PostgreSQL mediante algún servidor  (En nuestro caso utilizamos Pampero) y una terminal o consola para generar comandos de forma local
+
+En primer lugar tenemos que crear una carpeta local llamada “BD1-Grupo2”
+	$ mkdir ./BD1-Grupo2
+	$ cd BD1-Grupo2
+
+Una vez dentro, tenemos que descargar los archivos .csv y el archivo tables.sql provisto por los alumnos, el contenido debería resultar el siguiente
+
+	$ ls
+clientes_banco.csv  pagos_cuotas.csv  prestamos_banco.csv  tables.sql
+
+Desde la terminal pasar los .csv y .sql pampero
+        $ scp -r ./BD1-Grupo2 gfrancois@pampero.itba.edu.ar:./
+
+Conectarse con Pampero
+       $ ssh usuario@pampero.itba.edu.ar -L 8888:bd1.it.itba.edu.ar:5432
+
+Revisar que estén los documentos que intentamos pasar
+        $ cd BD1-Grupo2
+        $ ls
+clientes_banco.csv  pagos_cuotas.csv  prestamos_banco.csv tables.sql
+
+Correr el archivo .sql para generar las tablas y generar el trigger
+        $ psql -h bd1.it.itba.edu.ar -U usuario -f tables.sql PROOF
+
+Conectarse con postgresql
+        $ psql -h bd1.it.itba.edu.ar -U usuario PROOF
+
+Copiar los datos de los .csv a las tablas previamente creadas
+	\copy cliente_tp(Codigo,Dni,Telefono,Nombre,Direccion) FROM clientes_banco.csv csv delimiter ',' header;
+
+	\copy prestamo_tp(Codigo,Fecha,cod_cliente,Importe) FROM prestamos_banco.csv csv delimiter ',' header;
+
+	\copy cuota_tp(numero_couta,cod_prestamo,Importe,Fecha) FROM pagos_cuotas.csv csv delimiter ',' header;
+
+para verificar que se cargaron los datos realizamos un comando sql como:
+select *
+from cliente_tp;
+select *
+from cuota_tp;
+select *
+from prestamo_tp;
+
+salir de postgresql
+	$ exit
+
+salir de la carpeta
+	$ cd ..
+
+Mantenerse en pampero si se quiere acceder a tablas desde DataGrip
+ */
+
+-- --------------------------------------------------------------------------------------------
+-- TABLA PARA Cliente_banco.csv
+-- --------------------------------------------------------------------------------------------
+CREATE TABLE cliente_tp
+(
+    dni       TEXT NOT NULL,
+    nombre    TEXT,
+    direccion TEXT,
+    telefono  TEXT,
+    codigo    INT check ( codigo > 0 ) PRIMARY KEY,
+    UNIQUE (dni)
+);
+
+
+-- --------------------------------------------------------------------------------------------
+-- TABLA PARA Prestamo_banco.csv
+-- --------------------------------------------------------------------------------------------
+CREATE TABLE prestamo_tp
+(
+    importe INT check ( importe > 0 ),
+    codigo INT check ( codigo > 0 ) PRIMARY KEY ,
+    fecha DATE,
+    cod_cliente INT,
+    FOREIGN KEY (cod_cliente) REFERENCES cliente_tp(codigo) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+-- --------------------------------------------------------------------------------------------
+-- TABLA PARA pagos_cuota.csv
+-- --------------------------------------------------------------------------------------------
+CREATE TABLE cuota_tp
+(
+    numero_cuota INT check ( numero_cuota > 0 ),
+    importe INT check ( importe > 0 ),
+    fecha DATE,
+    cod_prestamo INT,
+    PRIMARY KEY (numero_cuota,cod_prestamo),
+    FOREIGN KEY (cod_prestamo) REFERENCES prestamo_tp(codigo) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+-- --------------------------------------------------------------------------------------------
+-- TABLA PARA manejo del Trigger
+-- --------------------------------------------------------------------------------------------
+CREATE TABLE backup_tp
+(
+    dni TEXT,
+    nombre TEXT,
+    telefono TEXT,
+    cant_prestamos INT check ( cant_prestamos >= 0 ),
+    monto_prestamos INT check ( monto_prestamos >= 0 ),
+    monto_pago_cuotas INT check ( monto_pago_cuotas >= 0 ),
+    ind_pagos_pendientes BOOLEAN,
+    fecha TIMESTAMP,
+    PRIMARY KEY (dni,fecha)
+);
+
+
 -- --------------------------------------------------------------------------------------------
 -- monto_de_prestamo_pagado: Dado el codigo de un prestamo, obtiene el monto pagado hasta el momento de dicho prestamo
 -- --------------------------------------------------------------------------------------------
